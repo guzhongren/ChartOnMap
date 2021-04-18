19f51b5923e32ba927b93c5c81d796cd246a8f90
guzhongren <guzhongren@live.cn>
2021-04-19 07:30:39 +0800

    feat(demo): add mew demo

---
+++ examples/demo-data.js
@@ -0,0 +1,105 @@
+const result = {
+  fields: [
+      {
+          name: 'name1',
+          type: 'string'
+      },
+  
+      {
+          name: 'name2',
+          type: 'string'
+      },
+      {
+          name: 'name3',
+          type: 'string'
+      },
+      {
+          name: 'name4',
+          type: 'string'
+      },
+      {
+          name: 'name5',
+          type: 'string'
+      },
+      {
+          name: 'name6',
+          type: 'string'
+      },
+      {
+          name: 'name6',
+          type: 'string'
+      },
+      {
+          name: 'name7',
+          type: 'string'
+      },
+  ],
+  features: [
+          {
+              attributes: {
+                  name7: 10,
+                  name6: 120,
+                  name5: 150,
+                  name4: 199,
+                  name3: 399,
+              },
+              center: {
+                  x: 112,
+                  y: 45
+              }
+          },
+          {
+              attributes: {
+                  name7: 120,
+                  name6: 10,
+                  name5: 160,
+                  name4: 199,
+                  name3: 889,
+              },
+              center: {
+                  x: 114,
+                  y: 25
+              }
+          },
+          {
+              attributes: {
+                  name7: 10,
+                  name6: 90,
+                  name5: 30,
+                  name4: 299,
+                  name3: 999,
+              },
+              center: {
+                  x: 117,
+                  y: 45
+              }
+          },
+
+          {
+              attributes: {
+                  name7: 10,
+                  name6: 170,
+                  name5: 340,
+                  name4: 269,
+                  name3: 989,
+              },
+              center: {
+                  x: 112,
+                  y: 40
+              }
+          },
+          {
+              attributes: {
+                  name7: 100,
+                  name6: 280,
+                  name5: 874,
+                  name4: 468,
+                  name3: 245,
+              },
+              center: {
+                  x: 102,
+                  y: 45
+              }
+          },
+      ]
+}
